bd914b67f85118247b1dd7f6de175acfb1e6f1a4
Amanda Mok <a.mok@berkeley.edu>
2017-12-07 11:56:48 -0800

    more updates for weights in nextGen()

--- GA/R/Next Generation.R
+++ GA/R/Next Generation.R
@@ -29,7 +29,7 @@ nextGen = function(pop, pSelect=0.2, pMutate=0.01, fitfunc="AIC", family="gaussi
   if(length(which(weights == 1)) != 0)  return(pop)
   ##########Might wanna change our method here??################
   nChrom = length(oldGenomes)
-  weights = rank(getFitness(oldGenomes))/(nChrom*(nChrom+1))
+  weights = 2*order(getFitness(oldGenomes), decreasing=T)/(nChrom*(nChrom+1))
   newGenomes = lapply(1:numRemove,
                       function(x) {
                         crossover(sample(oldGenomes, size=2, prob=weights),
